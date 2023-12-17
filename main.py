c205ca6e6274d6089d7d4196fc839847c51d9d26
Luewngier <g217881111@gmail.com>
2023-12-17 20:30:19 +0800

    Read column by pandas lib

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 #     data = data_file.readlines()
 #     print(data)
 
-import csv
+# import csv
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if (row[1]) != "temp":
-            temperatures.append(int(row[1]))
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if (row[1]) != "temp":
+#             temperatures.append(int(row[1]))
 
-print(temperatures)
+# print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
