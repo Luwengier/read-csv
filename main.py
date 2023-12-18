a739b442d8fa7d238b1f7023690824d38d62b311
Luewngier <g217881111@gmail.com>
2023-12-18 23:36:22 +0800

    Convert row data into list

--- main.py
+++ main.py
@@ -17,5 +17,8 @@ import pandas
 
 data = pandas.read_csv("weather_data.csv")
 
-data_dict = data.to_dict()
-print(data_dict)
+# data_dict = data.to_dict()
+# print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
