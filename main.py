02457690d7ea611e23db5c259d0b0f8a595052c8
Luewngier <g217881111@gmail.com>
2023-12-19 23:41:20 +0800

    Get mean and max of temperature

--- main.py
+++ main.py
@@ -20,5 +20,8 @@ data = pandas.read_csv("weather_data.csv")
 # data_dict = data.to_dict()
 # print(data_dict)
 
-temp_list = data["temp"].to_list()
-print(temp_list)
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+
+print(data["temp"].mean())
+print(data["temp"].max())
