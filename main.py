f1325d5868414eb3b4c61b5c825d885675ba1165
Luewngier <g217881111@gmail.com>
2023-12-21 23:49:50 +0800

    Create DataFrame from scratch

--- main.py
+++ main.py
@@ -29,7 +29,12 @@ data = pandas.read_csv("weather_data.csv")
 # print(data[data.day == "Monday"])
 # print(data[data.temp == data.temp.max()])
 
-monday = data[data.day == "Monday"]
-monday_temp = monday.temp[0]
-monday_temp_F = monday_temp * 9 / 5 + 32
-print(monday_temp_F)
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp[0]
+# monday_temp_F = monday_temp * 9 / 5 + 32
+# print(monday_temp_F)
+
+data_dict = {"students": ["Amy", "James", "Angela"], "scores": [76, 56, 65]}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
