a1c5293d9e516987f3cc4fae398daa7e4ad5eddb
Luewngier <g217881111@gmail.com>
2023-12-23 21:42:08 +0800

    Analysis fur color of squirrels and export to csv

--- main.py
+++ main.py
@@ -15,7 +15,28 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+data = pandas.read_csv("squirrel_data.csv")
+
+gray = 0
+cinnamon = 0
+black = 0
+
+for color in data["Primary Fur Color"]:
+    if color == "Gray":
+        gray += 1
+    elif color == "Cinnamon":
+        cinnamon += 1
+    elif color == "Black":
+        black += 1
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray, cinnamon, black],
+}
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
+
+# data = pandas.read_csv("weather_data.csv")
 
 # data_dict = data.to_dict()
 # print(data_dict)
@@ -34,7 +55,7 @@ data = pandas.read_csv("weather_data.csv")
 # monday_temp_F = monday_temp * 9 / 5 + 32
 # print(monday_temp_F)
 
-data_dict = {"students": ["Amy", "James", "Angela"], "scores": [76, 56, 65]}
+# data_dict = {"students": ["Amy", "James", "Angela"], "scores": [76, 56, 65]}
 
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
